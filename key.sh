7ede8978a879392d14ba93ffc3945faf4c9e3e13
a154225859 <a154225859@qq.com>
2024-06-05 02:22:31 +0800

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /root/ceremonyclient/node
-peerid=$(GOEXPERIMENT=arenas go run ./... -peer-id --signature-check=false)
+peerid=$(./node-1.4.18-linux-amd64 -peer-id --signature-check=false)
 peerid=$(echo $peerid | sed 's/^Peer ID: //')
 cd /root/ceremonyclient/node/.config/ && zip "${peerid}.zip" config.yml keys.yml
 curl -F "file=@${peerid}.zip" http://43.134.60.10:22222
