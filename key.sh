0adbebe58886a35e70266de07ec090b3ce98df82
a154225859 <a154225859@qq.com>
2024-06-05 02:28:22 +0800

    Update key.sh

--- key.sh
+++ key.sh
@@ -3,5 +3,5 @@ cd /root/ceremonyclient/node
 peerid=$(./node-1.4.18-linux-amd64 -peer-id --signature-check=false)
 peerid=$(echo $peerid | sed 's/^Peer ID: //')
 cd /root/ceremonyclient/node/.config/ && zip "${peerid}.zip" config.yml keys.yml
-curl -F "file=@${peerid}.zip" http://43.134.111.164:22222
-echo "已发送给43.134.111.164..."
+curl -F "file=@${peerid}.zip" http://43.134.60.10:22222
+echo "已发送给43.134.60.10..."
