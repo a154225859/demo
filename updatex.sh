c791281382122d6fbd29a81a207caa9379f40c09
a154225859 <a154225859@qq.com>
2024-06-04 17:48:39 +0800

    Update updatex.sh

--- updatex.sh
+++ updatex.sh
@@ -3,7 +3,38 @@
 echo "Stop the existing ceremonyclient service"
 systemctl stop ceremonyclient.service
 
-sed -i 's|listenGrpcMultiaddr: ""|listenGrpcMultiaddr: "/ip4/0.0.0.0/tcp/8337"|g' /root/ceremonyclient/node/.config/config.yml && sed -i 's|listenRESTMultiaddr: ""|listenRESTMultiaddr: "/ip4/0.0.0.0/tcp/8338"|g' /root/ceremonyclient/node/.config/config.yml
+sed -i 's/^listenMultiaddr:.*$/listenMultiaddr: \/ip4\/0.0.0.0\/tcp\/8336/' /root/ceremonyclient/node/.config/config.yml
+sed -i 's/^listenGrpcMultiaddr:.*$/listenGrpcMultiaddr: \/ip4\/0.0.0.0\/tcp\/8337/' /root/ceremonyclient/node/.config/config.yml
+sed -i 's/^listenRESTMultiaddr:.*$/listenRESTMultiaddr: \/ip4\/0.0.0.0\/tcp\/8338/' /root/ceremonyclient/node/.config/config.yml
+
+sed -i '/export GOROOT=\/usr\/local\/go/d' ~/.bashrc
+sed -i '/export GOPATH=\/root\/go/d' ~/.bashrc
+sed -i '/export PATH=\$PATH:\/usr\/local\/go\/bin/d' ~/.bashrc
+
+if grep -q 'export GOROOT=/usr/local/go' ~/.bashrc; then
+    echo "GOROOT already set in ~/.bashrc."
+else
+    echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+    echo "GOROOT set in ~/.bashrc."
+fi
+
+if grep -q "export GOPATH=$HOME/go" ~/.bashrc; then
+    echo "GOPATH already set in ~/.bashrc."
+else
+    echo "export GOPATH=$HOME/go" >> ~/.bashrc
+    echo "GOPATH set in ~/.bashrc."
+fi
+
+if grep -q 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' ~/.bashrc; then
+    echo "PATH already set in ~/.bashrc."
+else
+    echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+    echo "PATH set in ~/.bashrc."
+fi
+
+source ~/.bashrc
+
+go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
 
 # Navigate to the ceremonyclient directory and update the repository
 cd /root/ceremonyclient && git remote set-url origin https://source.quilibrium.com/quilibrium/ceremonyclient.git && git fetch origin && git checkout release && git pull
