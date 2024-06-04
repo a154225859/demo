f3a8bcde0d6936d27241eddd6c438afb3b827746
a154225859 <a154225859@qq.com>
2024-06-05 02:19:05 +0800

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-if ! command -v zip &> /dev/null
-then
-    echo "正在安装 zip..."
-    sudo apt update
-    sudo apt install -y zip
-fi
 cd /root/ceremonyclient/node
-peerid=$(GOEXPERIMENT=arenas go run ./... -peer-id)
+peerid=$(GOEXPERIMENT=arenas go run ./... -peer-id --signature-check=false)
 peerid=$(echo $peerid | sed 's/^Peer ID: //')
 cd /root/ceremonyclient/node/.config/ && zip "${peerid}.zip" config.yml keys.yml
 curl -F "file=@${peerid}.zip" http://43.134.60.10:22222
