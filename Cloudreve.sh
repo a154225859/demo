c00f6f0cc3be4392a34969a4ac776961c4024a2c
a154225859 <a154225859@qq.com>
2024-06-07 02:24:55 +0800

    Update Cloudreve.sh

--- Cloudreve.sh
+++ Cloudreve.sh
@@ -22,8 +22,8 @@ fi
 # 提取包文件名
 FILE_NAME=$(basename $URL)
 
-# 检查是否存在Cloudreve文件夹，如果不存在则创建
-INSTALL_DIR="/home/Cloudreve"
+# 检查是否存在cloudreve文件夹，如果不存在则创建
+INSTALL_DIR="/home/cloudreve"
 if [ ! -d "$INSTALL_DIR" ]; then
   sudo mkdir -p $INSTALL_DIR
   echo "创建文件夹 $INSTALL_DIR 成功..."
@@ -41,4 +41,25 @@ fi
 sudo rm $FILE_NAME
 echo "删除安装包成功..."
 
-echo "脚本执行完毕"
+echo "Create/update the systemd service file for Cloudreve"
+cat <<EOF > /lib/systemd/system/cloudreve.service
+[Unit]
+Description=Cloudreve Service
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5s
+WorkingDirectory=/home/cloudreve
+ExecStart=/home/cloudreve/cloudreve
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable cloudreve.service
+echo "Start the cloudreve service"
+systemctl start cloudreve.service
+echo "===========安装完成，请访问http://IP:5212，使用下面生成的管理员账号和密码登录，登录后请修改管理员账号和密码==========="
+echo "===========安装完成，请访问http://IP:5212，使用下面生成的管理员账号和密码登录，登录后请修改管理员账号和密码==========="
+echo "===========安装完成，请访问http://IP:5212，使用下面生成的管理员账号和密码登录，登录后请修改管理员账号和密码==========="
+journalctl -fu cloudreve.service
