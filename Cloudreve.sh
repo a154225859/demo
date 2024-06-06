79540f8612996932c02dbf558bd379e4325e3a75
a154225859 <a154225859@qq.com>
2024-06-07 02:16:38 +0800

    Update Cloudreve.sh

--- Cloudreve.sh
+++ Cloudreve.sh
@@ -1,33 +1,44 @@
+#!/bin/bash
+
 # 检查系统架构
-  ARCH=$(uname -m)
-  if [[ "$ARCH" == "x86_64" ]]; then
-    URL="http://49.13.194.189:8008/go1.20.14.linux-amd64.tar.gz"
-  elif [[ "$ARCH" == "aarch64" ]]; then
-    URL="http://49.13.194.189:8008/go1.20.14.linux-arm64.tar.gz"
-  else
-    echo "不支持的架构: $ARCH"
-    exit 1
-  fi
-
-  # 下载安装包
-  if wget -4 $URL; then
-    echo "下载安装包成功..."
-  else
-    echo "下载安装包失败..."
-    exit 1
-  fi
-  
-  # 提取包文件名
-  FILE_NAME=$(basename $URL)
-  
-  # 解压安装包
-  if sudo tar -C /home/Cloudreve -xzf $FILE_NAME; then
-    echo "解压安装包成功..."
-  else
-    echo "解压安装包失败..."
-    exit 1
-  fi
-
-  # 删除安装包
-  sudo rm $FILE_NAME
+ARCH=$(uname -m)
+if [[ "$ARCH" == "x86_64" ]]; then
+  URL="https://github.com/cloudreve/Cloudreve/releases/download/3.8.3/cloudreve_3.8.3_linux_amd64.tar.gz"
+elif [[ "$ARCH" == "aarch64" ]]; then
+  URL="https://github.com/cloudreve/Cloudreve/releases/download/3.8.3/cloudreve_3.8.3_linux_arm64.tar.gz"
+else
+  echo "不支持的架构: $ARCH"
+  exit 1
 fi
+
+# 下载安装包
+if wget -4 $URL; then
+  echo "下载安装包成功..."
+else
+  echo "下载安装包失败..."
+  exit 1
+fi
+
+# 提取包文件名
+FILE_NAME=$(basename $URL)
+
+# 检查是否存在Cloudreve文件夹，如果不存在则创建
+INSTALL_DIR="/home/Cloudreve"
+if [ ! -d "$INSTALL_DIR" ]; then
+  sudo mkdir -p $INSTALL_DIR
+  echo "创建文件夹 $INSTALL_DIR 成功..."
+fi
+
+# 解压安装包
+if sudo tar -C $INSTALL_DIR -xzf $FILE_NAME; then
+  echo "解压安装包成功..."
+else
+  echo "解压安装包失败..."
+  exit 1
+fi
+
+# 删除安装包
+sudo rm $FILE_NAME
+echo "删除安装包成功..."
+
+echo "脚本执行完毕"
