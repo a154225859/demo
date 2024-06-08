bb6728f6fcd5ba46feb17a8fc1970b87fdbc19ef
a154225859 <a154225859@qq.com>
2024-06-08 21:06:30 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,45 +3,6 @@
 echo "Stop the existing ceremonyclient service"
 systemctl stop ceremonyclient.service
 
-sed -i 's|listenMultiaddr: /ip4/0.0.0.0/udp/8336/quic|listenMultiaddr: /ip4/0.0.0.0/tcp/8336|g' /root/ceremonyclient/node/.config/config.yml
-sed -i 's|listenGrpcMultiaddr: ""|listenGrpcMultiaddr: "/ip4/0.0.0.0/tcp/8337"|g' /root/ceremonyclient/node/.config/config.yml
-sed -i 's|listenRESTMultiaddr: ""|listenRESTMultiaddr: "/ip4/0.0.0.0/tcp/8338"|g' /root/ceremonyclient/node/.config/config.yml
-
-sed -i '/export GOROOT=\/usr\/local\/go/d' ~/.bashrc
-sed -i '/export GOPATH=\/root\/go/d' ~/.bashrc
-sed -i '/export PATH=\$PATH:\/usr\/local\/go\/bin/d' ~/.bashrc
-
-if grep -q 'export GOROOT=/usr/local/go' ~/.bashrc; then
-    echo "GOROOT already set in ~/.bashrc."
-else
-    echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
-    echo "GOROOT set in ~/.bashrc."
-fi
-
-if grep -q "export GOPATH=$HOME/go" ~/.bashrc; then
-    echo "GOPATH already set in ~/.bashrc."
-else
-    echo "export GOPATH=$HOME/go" >> ~/.bashrc
-    echo "GOPATH set in ~/.bashrc."
-fi
-
-if grep -q 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' ~/.bashrc; then
-    echo "PATH already set in ~/.bashrc."
-else
-    echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
-    echo "PATH set in ~/.bashrc."
-fi
-
-source ~/.bashrc
-
-# 临时设置 Go 环境变量 - 解决一些奇怪的机器提示找不到go的问题
-sleep 1
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-
-go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
-
 # Navigate to the ceremonyclient directory and update the repository
 cd /root/ceremonyclient && git remote set-url origin https://source.quilibrium.com/quilibrium/ceremonyclient.git && git fetch origin && git checkout release-cdn && git pull
 
