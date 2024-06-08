6af16c0e113c888e82fd4fb38f95d548e932648f
a154225859 <a154225859@qq.com>
2024-06-08 21:05:03 +0800

    Update updatex.sh

--- updatex.sh
+++ updatex.sh
@@ -43,7 +43,7 @@ export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
 
 # Navigate to the ceremonyclient directory and update the repository
-cd /root/ceremonyclient && git remote set-url origin https://source.quilibrium.com/quilibrium/ceremonyclient.git && git fetch origin && git checkout release && git pull
+cd /root/ceremonyclient && git remote set-url origin https://source.quilibrium.com/quilibrium/ceremonyclient.git && git fetch origin && git checkout release-cdn && git pull
 
 # Extract version from Go file
 version=$(cat /root/ceremonyclient/node/config/version.go | grep -A 1 "func GetVersion() \[\]byte {" | grep -Eo '0x[0-9a-fA-F]+' | xargs printf "%d.%d.%d")
