fee7437d3d25cd76444f65d101d99c946a528eee
a154225859 <a154225859@qq.com>
2025-01-05 18:51:39 +0800

    Create reset_opl.sh

---
+++ reset_opl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 停止并移除 Docker 容器
+docker stop opl_worker
+docker stop opl_scraper
+docker rm opl_worker
+docker rm opl_scraper
+
+# 删除 machine-id 文件并重新生成
+rm -f /etc/machine-id
+systemd-machine-id-setup
+
+# 切换到指定目录并启动 Docker Compose 服务
+cd /root/opl/ || exit 1
+docker-compose up -d
